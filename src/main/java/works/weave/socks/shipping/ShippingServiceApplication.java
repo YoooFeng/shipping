10c29ea2f44a4abf24c2b5c1589abdadf6e2a216
YangFeng <250970437@qq.com>
2018-06-13 15:53:34 +0800

    Update ShippingServiceApplication.java

--- src/main/java/works/weave/socks/shipping/ShippingServiceApplication.java
+++ src/main/java/works/weave/socks/shipping/ShippingServiceApplication.java
@@ -2,7 +2,7 @@ package works.weave.socks.shipping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// 
 @SpringBootApplication
 public class ShippingServiceApplication {
     public static void main(String[] args) throws InterruptedException {
